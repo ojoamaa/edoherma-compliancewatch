0b426cf2438562d7a41265463d2f1875b1685ae8
ojoamaa <ojoamaa@gmail.com>
2026-03-21 01:52:23 +0100

    fix personnel default login credentials

--- edoherma-frontend/src/LoginPage.jsx
+++ edoherma-frontend/src/LoginPage.jsx
@@ -8,8 +8,8 @@ const DEFAULT_ADMIN = {
 };
 
 const DEFAULT_PERSONNEL = {
-    email: "",
-    password: "",
+    email: "osagie@edoherma.com",
+    password: "Password123!",
 };
 
 export default function LoginPage({ onLoginSuccess }) {
