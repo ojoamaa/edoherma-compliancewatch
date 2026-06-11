c3222ed19cc1ba25116d0032ea901719b9a6e997
ojoamaa <ojoamaa@gmail.com>
2026-06-11 10:43:59 +0100

    Add database table creation script

--- create_tables.py
+++ create_tables.py
@@ -1,3 +1,9 @@
+import app.models.admin
+import app.models.personnel
+import app.models.facility
+import app.models.compliance_alert
+import app.models.reminder_log
+
 from app.core.database import Base, engine
 
 print("Creating tables...")
